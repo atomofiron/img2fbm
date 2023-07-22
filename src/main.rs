146da8d049992b50083a7b3a685535c253752c7f
atomofiron <atomofiron@gmail.com>
2023-07-22 15:16:14 +0400

    move unused code to unused.rs

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,8 @@ use std::hash::{Hash, Hasher};
 use std::io::{Read, Write};
 use std::path::Path;
 use clap::{Parser, Arg, ArgAction, ArgMatches, Command, CommandFactory};
-use image::{ColorType, Delay, DynamicImage, Frame, GenericImage, GenericImageView, GrayImage, ImageFormat, Luma, Rgba};
+use image::{ColorType, Delay, DynamicImage, Frame, GrayImage, ImageFormat, Luma};
 use crate::core::bitmap::Bitmap;
-use crate::core::color::Color;
 use crate::core::args::Cli;
 use crate::core::img2bm::img2bm;
 
@@ -147,120 +146,4 @@ fn save_preview(img: &GrayImage, name: &str) {
         ColorType::L8,
         ImageFormat::Png,
     ).unwrap();
-}
-
-// unused:
-
-fn try_get_pixel<T>(image: &T, x: i32, y: i32) -> Option<T::Pixel>
-    where T: GenericImage
-{
-    match () {
-        _ if x < 0 => None,
-        _ if y < 0 => None,
-        _ if x >= image.width() as i32 => None,
-        _ if y >= image.height() as i32 => None,
-        _ => Some(image.get_pixel(x as u32, y as u32))
-    }
-}
-
-fn for_each<T, F>(image: &T, action: F)
-    where
-        T: GenericImage,
-        F: Fn(u32, u32, T::Pixel) /*+ Copy*/,
-{
-    for y in 0..image.height() {
-        for x in 0..image.width() {
-            action(x, y, image.get_pixel(x, y));
-        }
-    }
-}
-
-fn for_each_mut<T, F>(image: &mut T, action: F)
-    where
-        T: GenericImage,
-        F: Fn(&mut T, T::Pixel) -> Option<T::Pixel> /*+ Copy*/,
-{
-    for y in 0..image.height() {
-        for x in 0..image.width() {
-            let new = action(image, image.get_pixel(x, y));
-            if let Some(pixel) = new {
-                image.put_pixel(x, y, pixel)
-            }
-        }
-    }
-}
-
-fn abs_dif(first: u8, second: u8) -> u8 {
-    let mut dif = first as i16 - second as i16;
-    dif *= dif.signum();
-    dif as u8
-}
-
-fn copy_line(image: &DynamicImage, y: u32) -> Vec<[u8; 4]> {
-    let mut line = Vec::new();
-    for x in 0..image.width() {
-        line.push(image.get_pixel(x, y).0)
-    }
-    return line;
-}
-
-fn disagreement(image: &mut DynamicImage) {
-    let src = image.clone();
-    for y in 0..image.height() {
-        for x in 0..image.width() {
-            let current = image.get_pixel(x, y).0;
-            let mut dif_r: i32 = 0;
-            let mut dif_g: i32 = 0;
-            let mut dif_b: i32 = 0;
-            for dy in -1..1 {
-                let y = y as i32 + dy;
-                for dx in -1..1 {
-                    let x = x as i32 + dx;
-                    if dx == 0 && dy == 0 { continue; }
-                    if let Some(pixel) = try_get_pixel(&src, x, y) {
-                        let pixel = pixel.0;
-                        dif_r += current[0] as i32 - pixel[0] as i32;
-                        dif_g += current[1] as i32 - pixel[1] as i32;
-                        dif_b += current[2] as i32 - pixel[2] as i32;
-                    }
-                }
-            }
-            let max_dif = dif_r.max(dif_g).max(dif_b);
-            let new: [u8; 4] = [
-                ((255 - current[0] as i32) * (max_dif / 255) + current[0] as i32) as u8,
-                ((255 - current[1] as i32) * (max_dif / 255) + current[1] as i32) as u8,
-                ((255 - current[2] as i32) * (max_dif / 255) + current[2] as i32) as u8,
-                current[3],
-            ];
-            image.put_pixel(x, y, Rgba(new))
-        }
-    }
-}
-
-fn remove_background(image: &mut DynamicImage, color: Color, to_visible: bool) {
-    let target: u8 = if to_visible { 0 } else { 255 };
-    for_each_mut(image, |image, pixel| {
-        let r = pixel[0];
-        let g = pixel[1];
-        let b = pixel[2];
-        match () {
-            _ if abs_dif(color.r, r) > 50 => None,
-            _ if abs_dif(color.g, g) > 50 => None,
-            _ if abs_dif(color.b, b) > 50 => None,
-            _ if abs_dif(abs_dif(color.r, color.g), abs_dif(r, g)) > 30 => None,
-            _ if abs_dif(abs_dif(color.g, color.b), abs_dif(g, b)) > 30 => None,
-            _ => Some(Rgba([target, target, target, pixel.0[3]]))
-        }
-    });
-}
-
-fn color_to_u32(color: &str) -> u32 {
-    if color.len() != 6 && color.len() != 8 {
-        panic!("Color must contains 6 or 8 0..f-chars ({})", color);
-    }
-    let mut color_int = u32::from_str_radix(color, 16).unwrap();
-    if color.len() == 6 {
-        color_int += 0xff000000;
-    }
-    return color_int;
 }
---
+++ src/unused.rs
@@ -0,0 +1,76 @@
+use image::{DynamicImage, GenericImage, Rgba};
+use crate::core::color::Color;
+
+fn try_get_pixel<T>(image: &T, x: i32, y: i32) -> Option<T::Pixel>
+    where
+        T: GenericImage
+{
+    match () {
+        _ if x < 0 => None,
+        _ if y < 0 => None,
+        _ if x >= image.width() as i32 => None,
+        _ if y >= image.height() as i32 => None,
+        _ => Some(image.get_pixel(x as u32, y as u32))
+    }
+}
+
+fn for_each<T, F>(image: &T, action: F)
+    where
+        T: GenericImage,
+        F: Fn(u32, u32, T::Pixel) /*+ Copy*/,
+{
+    for y in 0..image.height() {
+        for x in 0..image.width() {
+            action(x, y, image.get_pixel(x, y));
+        }
+    }
+}
+
+fn for_each_mut<T, F>(image: &mut T, action: F)
+    where
+        T: GenericImage,
+        F: Fn(&mut T, T::Pixel) -> Option<T::Pixel> /*+ Copy*/,
+{
+    for y in 0..image.height() {
+        for x in 0..image.width() {
+            let new = action(image, image.get_pixel(x, y));
+            if let Some(pixel) = new {
+                image.put_pixel(x, y, pixel)
+            }
+        }
+    }
+}
+
+fn abs_dif(first: u8, second: u8) -> u8 {
+    let mut dif = first as i16 - second as i16;
+    dif *= dif.signum();
+    dif as u8
+}
+
+fn remove_background(image: &mut DynamicImage, color: Color, to_visible: bool) {
+    let target: u8 = if to_visible { 0 } else { 255 };
+    for_each_mut(image, |image, pixel| {
+        let r = pixel[0];
+        let g = pixel[1];
+        let b = pixel[2];
+        match () {
+            _ if abs_dif(color.r, r) > 50 => None,
+            _ if abs_dif(color.g, g) > 50 => None,
+            _ if abs_dif(color.b, b) > 50 => None,
+            _ if abs_dif(abs_dif(color.r, color.g), abs_dif(r, g)) > 30 => None,
+            _ if abs_dif(abs_dif(color.g, color.b), abs_dif(g, b)) > 30 => None,
+            _ => Some(Rgba([target, target, target, pixel.0[3]]))
+        }
+    });
+}
+
+fn color_to_u32(color: &str) -> u32 {
+    if color.len() != 6 && color.len() != 8 {
+        panic!("Color must contains 6 or 8 0..f-chars ({})", color);
+    }
+    let mut color_int = u32::from_str_radix(color, 16).unwrap();
+    if color.len() == 6 {
+        color_int += 0xff000000;
+    }
+    return color_int;
+}
